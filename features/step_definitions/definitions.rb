8e866a45118032c3195346cbd3f1a6e4869a101d
Samuel Albrecht <elsamuko@gmail.com>
2021-01-06 22:20:23 +0100

    add: tests

---
+++ features/step_definitions/definitions.rb
@@ -0,0 +1,15 @@
+# regex syntax
+Given(/^the number (\d+)$/) do |arg1|
+  # keep arg1 in the global variable num
+  @num = arg1.to_i
+end
+
+# expression syntax
+# https://cucumber.io/docs/cucumber/cucumber-expressions/
+When("I add {float}") do |arg1|
+  @num += arg1
+end
+
+Then(/^I get (\d+\.\d+)$/) do |arg1|
+  expect(@num).to eq arg1.to_f
+end
---
+++ features/tests.feature
@@ -0,0 +1,8 @@
+Feature: This is a test compilation
+
+  This file must have the extension *.feature
+
+  Scenario: Name of Test
+    Given the number 5
+    When I add 4.4
+    Then I get 9.4
